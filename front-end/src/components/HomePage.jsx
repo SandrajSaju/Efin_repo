212769cc02c4c65c0f0e610bdd93a45ce956a6bc
Sandraj Saju <sandrajdevamangalam@gmail.com>
2024-05-26 18:28:28 +0530

    new function added

--- front-end/src/components/HomePage.jsx
+++ front-end/src/components/HomePage.jsx
@@ -29,6 +29,20 @@ const HomePage = () => {
     }
   }
 
+  const getGoodBooks = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:4000/book/getallbooks', {
+        params: {
+          page: currentPage
+        }
+      });
+      setBooks(data.books);
+      setTotalPages(data.totalPages);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   useEffect(() => {
     getAllBooks()
   }, [currentPage]);
